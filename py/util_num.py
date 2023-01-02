2286764011f9b9fb5cae1d1a835eb4745df887b6
cexen <cexenial@gmail.com>
2023-01-02 23:23:21 +0900

    py/util_num.py: add popcount63()

--- py/util_num.py
+++ py/util_num.py
@@ -1,4 +1,23 @@
 # https://github.com/cexen/procon-cexen/blob/main/py/util_num.py
+def popcount63(n: int) -> int:
+    """
+    Returns the number of 1 bits. Faster than popcount().
+    Required: 0 <= n < (1 << 63).
+    cf. https://nixeneko.hatenablog.com/entry/2018/03/04/000000
+    >>> popcount(0b11110001)
+    5
+    """
+    # Note: 63 bit is much faster than 64 bit on PyPy3 7.3.0
+    assert 0 <= n <= 0x7FFFFFFFFFFFFFFF  # this tells PyPy to use int64_t
+    c = (n & 0x5555555555555555) + ((n >> 1) & 0x5555555555555555)
+    c = (c & 0x3333333333333333) + ((c >> 2) & 0x3333333333333333)
+    c = (c & 0x0F0F0F0F0F0F0F0F) + ((c >> 4) & 0x0F0F0F0F0F0F0F0F)
+    c = (c & 0x00FF00FF00FF00FF) + ((c >> 8) & 0x00FF00FF00FF00FF)
+    c = (c & 0x0000FFFF0000FFFF) + ((c >> 16) & 0x0000FFFF0000FFFF)
+    c = (c & 0x00000000FFFFFFFF) + ((c >> 32) & 0x00000000FFFFFFFF)
+    return c
+
+
 def popcount(n: int) -> int:
     """
     Returns the number of 1 bits.
