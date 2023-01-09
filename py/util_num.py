f6a5c2282e4fda020a3c88ccfc875fd2e0964e11
cexen <cexenial@gmail.com>
2023-01-09 11:10:38 +0900

    py/util_num.py: improve discrete_log() for arbitrary mod

--- py/util_num.py
+++ py/util_num.py
@@ -300,34 +300,73 @@ def inv(a: int, mod: int) -> int:
     return ia % mod if d == 1 else 0
 
 
-from math import ceil, sqrt
+from math import ceil, sqrt, gcd
 from typing import Dict
 
 
-def discrete_log(a: int, b: int, m: int) -> int:
+def discrete_log(a: int, b: int, m: int, m_is_prime: bool) -> int:
     """
     O(m**0.5). Baby-step giant-step.
-    Required: gcd(a, m) == 1.
+    v1.1 @cexen.
     Returns min x s.t. 0<=x<m and a**x=b mod m.
     Returns -1 if nonexistent.
+    Works ok with gcd(a, m) != 1.
+    Note that 0**0 == 1.
+    cf. https://qiita.com/suisen_cp/items/d597c8ec576ae32ee2d7
     """
+    assert m >= 1
+    if m == 1:
+        return 0
+    a %= m
+    b %= m
+    if a == 0:
+        if b == 1:
+            return 0
+        elif b == 0:
+            return 1
+        else:
+            return -1
+
+    # reduce m to ensure gcd(a, m) == 1
+    d = m.bit_length() - 1
+    ax = 1
+    for x in range(d):
+        if ax == b:
+            return x
+        ax = ax * a % m
+    if ax == 0:
+        return d if b == 0 else -1
+    g = gcd(ax, m)
+    if b % g != 0:
+        return -1
+    m //= g
+    if m_is_prime:
+        iv = pow(ax, m - 2, m)
+    else:
+        iv = inv(ax, m)
+    assert iv != 0, "unreachable"
+    b = b * iv % m
+
+    # baby-step, giant-step
+    # assert gcd(a, m) == 1, "unreachable"
     k = ceil(sqrt(m))
     s: Dict[int, int] = {}
-    v = 1
-    for i in range(k):
-        if v not in s:
-            s[v] = i
-        v = v * a % m
-    iak = inv(v, m)
-    assert iak is not None, f"gcd(a, m) != 1"
-    v = 1
-    for i in range(k):
-        r = s.get(v * b % m, -1)
+    ax = 1
+    for x in range(k):
+        s.setdefault(ax, x)
+        ax = ax * a % m
+    if m_is_prime:
+        iak = pow(ax, m - 2, m)
+    else:
+        iak = inv(ax, m)
+    assert iak != 0, "unreachable"
+    iakx = 1
+    for x in range(k + 1):
+        r = s.get(iakx * b % m, -1)
         if r != -1:
-            return i * k + r
-        v = v * iak % m
-    r = s.get(v * b % m, -1)
-    return k * k + r if r != -1 else -1
+            return d + x * k + r
+        iakx = iakx * iak % m
+    return -1
 
 
 from typing import List, Tuple, Optional
@@ -361,3 +400,21 @@ def crt_naive(rs: List[int], ms: List[int]) -> Tuple[int, Optional[int]]:
 
 def crt_garner(rs: List[int], ms: List[int]) -> Tuple[int, Optional[int]]:
     raise NotImplementedError
+
+
+# --------------------
+
+
+def solve_yosupojudge_discrete_logarithm_mod():
+    """
+    Library Checker: Discrete Logarithm
+    https://judge.yosupo.jp/problem/discrete_logarithm_mod
+    """
+    T = int(input())
+    anses = []
+    for _ in range(T):
+        x, y, m = map(int, input().split())
+        ans = discrete_log(x, y, m, m_is_prime=False)
+        anses.append(ans)
+    for ans in anses:
+        print(ans)
