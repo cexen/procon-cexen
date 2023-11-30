5f135ed7af6bdd180fa9fce0613262819dfcf87f
cexen <cexenial@gmail.com>
2023-11-30 23:14:28 +0900

    py/unionfind.py: add UnionFindWithPotential

--- py/unionfind.py
+++ py/unionfind.py
@@ -67,6 +67,113 @@ class UnionFind:
             # weights[i] += weights[j]
 
 
+from typing import TypeVar, Generic, Protocol
+
+Self = TypeVar("Self")
+
+
+class SupportsAddSubNeg(Protocol):
+    def __add__(self: Self, other: Self, /) -> Self:
+        ...
+
+    def __sub__(self: Self, other: Self, /) -> Self:
+        ...
+
+    def __neg__(self: Self) -> Self:
+        ...
+
+
+_T = TypeVar("_T", bound=SupportsAddSubNeg)
+
+
+class UnionFindWithPotential(Generic[_T]):
+    """
+    v1.0 @cexen.
+    See UnionFindWithPotentialInt for usage.
+    cf. https://qiita.com/drken/items/cce6fc5c579051e64fab
+    """
+
+    def __init__(self, n: int, _0: _T):
+        self._data = [~1] * n
+        self._sizes = [1] * n
+        self._pots = [_0] * n
+
+    def __len__(self) -> int:
+        return len(self._data)
+
+    def size(self, i: int) -> int:
+        assert 0 <= i < len(self)
+        return self._sizes[self.root(i)]
+
+    def root(self, i: int) -> int:
+        assert 0 <= i < len(self)
+        if self._data[i] < 0:
+            return i
+        r = self._data[i]
+        self._data[i] = self.root(r)
+        self._pots[i] += self._pots[r]
+        return self._data[i]
+
+    def united(self, i: int, j: int) -> bool:
+        return self.root(i) == self.root(j)
+
+    def diff(self, i: int, j: int) -> _T:
+        """
+        YOU MUST ENSURE THAT self.united(i, j).
+        """
+        assert self.united(i, j)  # potentials are refreshed here
+        return self._pots[j] - self._pots[i]
+
+    def unite(self, i: int, j: int, diff: _T) -> bool:
+        """
+        YOU MUST ENSURE THAT not self.united(i, j) or self.diff(i, j) == diff.
+        Returns if newly united.
+        """
+        ri = self.root(i)
+        rj = self.root(j)
+        if ri == rj:
+            assert self.diff(i, j) == diff
+            return False
+        diff += self.diff(ri, i) + self.diff(j, rj)
+        del i, j
+        if ~self._data[ri] < ~self._data[rj]:
+            ri, rj = rj, ri
+            diff = -diff
+        elif ~self._data[ri] == ~self._data[rj]:
+            self._data[ri] = ~(~self._data[ri] + 1)
+        self._data[rj] = ri
+        self._sizes[ri] += self._sizes[rj]
+        self._pots[rj] += diff
+        return True
+
+
+class UnionFindWithPotentialInt(UnionFindWithPotential[int]):
+    """
+    >>> uf = UnionFindWithPotentialInt(5)
+    >>> uf.unite(0, 1, -5)
+    True
+    >>> uf.unite(1, 2, 3)
+    True
+    >>> uf.diff(0, 2)
+    -2
+    >>> uf.unite(0, 2, -2)
+    False
+    >>> uf.unite(2, 0, 2)
+    False
+    >>> uf.unite(0, 2, 3)
+    Traceback (most recent call last):
+        ...
+    AssertionError
+    >>> uf.diff(3, 4)
+    Traceback (most recent call last):
+        ...
+    AssertionError
+    """
+
+    def __init__(self, n: int):
+        return super().__init__(n, 0)
+
+
 # --------------------
 
 
@@ -88,3 +195,22 @@ def solve_yosupojudge_unionfind():
             raise RuntimeError
     for a in ans:
         print(int(a))
+
+
+def solve_atcoder_abc328_f():
+    """
+    AtCoder: abc328_f
+    https://atcoder.jp/contests/abc328/tasks/abc328_f
+    """
+    N, Q = map(int, input().split())
+    ans = list[int]()
+    uf = UnionFindWithPotentialInt(N)
+    for i in range(Q):
+        a, b, d = map(int, input().split())
+        a -= 1
+        b -= 1
+        if uf.united(a, b) and uf.diff(a, b) != d:
+            continue
+        uf.unite(a, b, d)
+        ans.append(i)
+    print(*(i + 1 for i in ans))
