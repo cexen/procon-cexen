8440b2733a9df41067989fad53b4c300cb6555f0
cexen <cexenial@gmail.com>
2024-07-12 23:41:25 +0900

    py/mod.py: re-implement _cache_ifacts(n) to be O(Δn)

--- py/mod.py
+++ py/mod.py
@@ -1,12 +1,12 @@
 # https://github.com/cexen/procon-cexen/blob/main/py/mod.py
 class Mod:
     """
-    v1.3 @cexen
+    v1.4 @cexen
     Expected to be faster than Mint.
     >>> mod = Mod(998244353)
     >>> mod.fact(10)  # factorial (auto-cached)
     3628800
-    >>> mod.ifact(2)  # inverse of factorial (auto-cached)
+    >>> mod.ifact(2)  # inverse of factorial (auto-cached) ; mod must be a prime
     499122177
 
     You may make cache manually (O(nmax)):
@@ -20,27 +20,45 @@ class Mod:
     6
     >>> mod.comb(-3, 3) - mod.val  # [x**3](1+x)**(-3)
     -10
-    >>> mod.div(12, 3)  # mod must be a prime here
+    >>> mod.div(12, 3)  # Required: gcm(3, mod) == 1.
     4
     """
 
-    KNOWN_PRIMES = {998244353, 1000000007, 1000000009}
+    KNOWN_PRIMES = {167772161, 469762049, 998244353, 1000000007, 1000000009, 1224736769}
 
     def __init__(
         self, mod: int = 998244353, nmax: int = 1, is_prime: bool | None = None
     ):
         """O(nmax)."""
         self.mod = mod
+        self.invs = [0, 1]  # invs[0] is unused
         self.facts = [1, 1]
         self.ifacts = [1, 1]
         self.is_prime = is_prime
         if is_prime is None and mod in self.KNOWN_PRIMES:
             self.is_prime = True
-        self._cache_ifacts(nmax)
+        self._cache_facts(nmax)
+        if self.is_prime:
+            self._cache_ifacts(nmax)
 
     @property
     def val(self) -> int:
         return self.mod
+
+    def _cache_invs(self, n: int) -> None:
+        """
+        O(Δn).
+        cf. https://drken1215.hatenablog.com/entry/2018/06/08/210000
+        """
+        assert self.is_prime
+        if n < len(self.invs):
+            return
+        mod = self.mod
+        invs = self.invs
+        i0 = len(invs)
+        invs.extend([1] * (n + 1 - i0))
+        for i in range(i0, n + 1):
+            invs[i] = mod - invs[mod % i] * (mod // i) % mod
 
     def _cache_facts(self, n: int) -> None:
         """O(Δn)."""
@@ -54,17 +72,18 @@ class Mod:
             facts[i] = facts[i - 1] * i % mod
 
     def _cache_ifacts(self, n: int) -> None:
-        """O(Δn + log n)."""
+        """O(Δn)."""
+        assert self.is_prime
         if n < len(self.ifacts):
             return
-        self._cache_facts(n)
+        self._cache_invs(n)
         mod = self.mod
+        invs = self.invs
         ifacts = self.ifacts
         i0 = len(ifacts)
         ifacts.extend([1] * (n + 1 - i0))
-        ifacts[n] = self.inv(self.facts[n])
-        for i in reversed(range(i0, n)):
-            ifacts[i] = ifacts[i + 1] * (i + 1) % mod
+        for i in range(i0, n + 1):
+            ifacts[i] = ifacts[i - 1] * invs[i] % mod
 
     def fact(self, v: int) -> int:
         self._cache_facts(v)
@@ -90,8 +109,10 @@ class Mod:
         return self.comb(n + r - 1, r)
 
     def inv(self, v: int) -> int:
-        """O(log v). mod must be a prime!"""
-        assert self.is_prime
+        """O(log v) if v > nmax or not is_prime. Required: gcd(v, mod) == 1."""
+        v %= self.mod
+        if 0 < v < len(self.invs):
+            return self.invs[v]
         return pow(v, -1, self.mod)
 
     def add(self, a: int, b: int) -> int:
